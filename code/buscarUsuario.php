d821f7620591c7af4aa81cd32a082c1483adad85
LuisReynoso <luis.reynoso233531@potros.itson.edu.mx>
2023-05-10 18:36:53 -0700

    xd

--- code/buscarUsuario.php
+++ code/buscarUsuario.php
@@ -3,31 +3,27 @@
     $nombre = $_GET["nombre"];
     $contra = $_GET["contrasenia"];
 
-if($nombre=="" or $contra==""){
-    echo '<script language="javascript">alert("Error de autentificacion");window.location.href="../index.html"</script>';
-    exit;
-}
-
-$verificarUsuario = "SELECT * FROM usuarios WHERE nombre = '$nombre' AND contrasenia ='$contra'";
-$verificacion = mysqli_query($conexion,$verificarUsuario);
-
-if(mysqli_num_rows($verificacion) == 0){
-    echo '<script language="javascript">alert("El usuario no existe");window.location.href="../index.html"</script>';
-    return;
-}
-    $codigoSQL = "SELECT nombre, contrasenia FROM usuarios WHERE nombre = '$nombre' AND contrasenia = '$contra'";
-    $resultSet = mysqli_query($conexion, $codigoSQL);
-
-     /* ESTO QUE ESTOY HACIENDO ES DESERIALIZACION INECESARIA POR LO QUE YA TENEMOS UNA VULNERABILIDAD */;
-     if($resultSet) {
-        while($row = $resultSet->fetch_assoc()) {
-            if($row["nombre"] == $nombre and $row["contrasenia"] == $contra ) {
-                echo "FELICIDADES SI INGRESO"."<br>";
-                echo $nombre;
-            }
-        }
+    if($nombre=="" or $contra==""){
+        echo '<script language="javascript">alert("Por favor llene todos los campos");window.location.href="../index.html"</script>';
+        exit;
     }
 
-    
+    $verificarUsuario = "SELECT * FROM usuarios WHERE nombre = '$nombre'";
+    $verificacion = mysqli_query($conexion,$verificarUsuario);
 
+    if(mysqli_num_rows($verificacion) == 0){
+        echo '<script language="javascript">alert("El usuario no existe");window.location.href="../index.html"</script>';
+        return;
+    }
+    $row = mysqli_fetch_assoc($verificacion);
+    $hash = $row['contrasenia'];
+
+    if(password_verify($contra, $hash)) {
+        echo '<script language="javascript">alert("Felicidades, ingreso");window.location.href="../paginas/sesion.php"</script>';
+        session_start();
+        $_SESSION["nombre"] = $row["nombre"];
+        $_SESSION["imagen"] = $row["imagen"];
+    } else {
+        echo '<script language="javascript">alert("La contraseña no es correcta");window.location.href="../index.html"</script>';
+    }
 ?>
